8f837884e62a65488491b88cd949d6411d543b66
TaylorMater <rt.taylor.riley@gmail.com>
2024-06-27 15:11:04 -0500

    Finished 7.034 - Data input and output

--- Section_07/7.034_Data_input_and_output/main.cpp
+++ Section_07/7.034_Data_input_and_output/main.cpp
@@ -1,11 +1,65 @@
 #include <iostream>
-//.
-consteval int get_value(){
-    return 3;
-}
+#include <string>
+#include <limits>
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value: " << value << std::endl;
+
+    // std::cout << "Hello C++20" << std::endl;
+    // int age {21};
+    // std::cout << "Age : " << age << std::endl;
+
+    // std::cerr << "Error message: Something is wrong" << std::endl;
+    // std::clog << "Log message: information here" << std::endl;
+    int option;
+    std::cout << "Enter option: " << std::endl;
+    std::cin >> option;
+
+    int age1;    
+    //needed to import string library, see import statements above
+    std::string name;
+
+
+    switch(option){
+    
+    //basic i/o
+    case 1:
+        std::cout << "Please type your name and age: " << std::endl;
+        std::cin >> name;
+        std::cin >> age1;
+        std::cout << "Hello " << name << " you are " << age1 << " years old. " << std::endl;
+        break;
+    case 2:
+    //now let's consider chaining inputs
+        std::cout << "Please type your name and age: " << std::endl;
+        std::cin >> name >> age1;
+        std::cout << "Hello " << name << " you are " << age1 << " years old. " << std::endl;
+        //But this won't work as expected if we put two names in for our name!
+        //notice, if you type "name1 name2 21" then we get "Hello name1 you are 0 years old". See the learncpp section on handling invalid input for more info.
+        break;
+    case 3:
+    //using the std::getline() function
+        std::cout << "Please enter your full name" << std::endl;
+
+        //note that std::getline() does not ignore leading whitespace. So it just reads the return character left from a previous std::cin extraction, and stops there. If we want to read more, we need to use the command std::cin.ignore() to flush that return character, or make a dummy getline() call before the one we are interested in. 
+
+        //this statement allows us to ignore all characters left in the stream, all the way to the max size of the stream, until we hit a return character, and then we extract that return and move on. This line allows us to clear space. Typically, we would want to add this to a utils file as a function called ignoreLine() or something, so we don't have to add this messy code all over the place. 
+        //Requires the limits library to be included
+    
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        std::getline(std::cin, name);
+        std::cout << "Your full name is " << name << std::endl;
+        break;
+    default:
+        break;
+    }
+
+
+
+
+
+
+
+
     return 0;
 }
--- Section_07/7.034_Data_input_and_output/notes.md
+++ Section_07/7.034_Data_input_and_output/notes.md
@@ -1,5 +1,11 @@
 ## Data Input and Output
 
+### Resources:
+
+https://www.learncpp.com/cpp-tutorial/introduction-to-iostream-cout-cin-and-endl/
+
+That is likely the best non-interactive C++ tutorial out there. Combined with this udemy course, you can master the fundamentals. Integration with network infrastructure, web design, and GUI stuff is something that will come later. 
+
 ### Basic io stream objects 
 
 The following gloabl objects are readily used to control input and output to a stream buffer. Not really sure what a stream buffer is exactly, but consider it more or less what is left to be processed in the console/terminal when your program runs. We'll learn more about it later.
@@ -36,7 +42,7 @@ You are <name_typed> and you are <age_typed> years old.
 
 ```
 
-- There would be errors of course if the user did not type in input that was relevant to the data types we expect: for example, a user writing `twenty-one` as their age would cause a run time error, because `twenty-one` is not of type `int`, it's a `std::string`. But we will read more on that later. 
+- There would be errors of course if the user did not type in input that was relevant to the data types we expect: for example, a user writing `twenty-one` as their age would cause a run time error, because `twenty-one` is not of type `int`, it's a `std::string`, and we would process that differently and the logic later would run into problems. It might not even break the program at first - see the learncpp.com site posted at the start of this file to see more. Also see [here](https://www.learncpp.com/cpp-tutorial/stdcin-and-handling-invalid-input/).
 
 
 
@@ -52,3 +58,20 @@ std::cout << "Type in your last name and age, separated by spaces: << std::endl;
 std::cin >> name >> age;
 
 ```
+
+If you need the user to input data which we want to extract that includes spaces, we want to use `std::getline(std::cin, var)` where `var` is the variable name which has been defined already. Example:
+
+```
+std::string fullName;
+std::cout << "Enter your full name: ";
+std::getline(std::cin, fullName);
+
+std::cout << fullName << std::endl;
+
+```
+
+The output of that code will be the text entered for your full name, including the spaces between your names. 
+
+
+
+Final note - `std::endl` is inefficient because it not only represents a new line character (`'\n'`) but it also flushes the buffer. We typically don't need to flush the buffer every line. If speed is important, you can default to just using `'\n'`, it is the preferred option from learncpp.com.
