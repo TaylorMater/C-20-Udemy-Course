b3c805138150232868e64a3091fae68702c68e98
TaylorMater <rt.taylor.riley@gmail.com>
2024-06-27 07:25:22 -0500

    Completed 7.030, beginning 7.031, copied template project to Section_07

---
+++ Section_07/7.030_Your-First_C++-Program/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!";
+    std::cout << "Riley Taylor 1" << std::endl;
+    std::cout << "Riley Taylor 2" << std::endl;
+    std::cout << "Riley Taylor 3" << std::endl;
+    std::cout << "Riley Taylor 4" << std::endl;
+    std::cout << "Riley Taylor 5" << std::endl;
+    std::cout << "Riley Taylor 6" << std::endl;
+    std::cout << "Riley Taylor 7" << std::endl;
+    std::cout << "Riley Taylor 8" << std::endl;
+    std::cout << "Riley Taylor 9" << std::endl;
+    std::cout << "Riley Taylor 10" << std::endl;
+
+
+    return 0;
+}
