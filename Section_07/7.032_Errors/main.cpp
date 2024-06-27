3697fc62ef5e907219e5bdde31311b7081be088a
TaylorMater <rt.taylor.riley@gmail.com>
2024-06-27 07:47:41 -0500

    Finished notes and classwork for 7.032 - Errors

--- Section_07/7.032_Errors/main.cpp
+++ Section_07/7.032_Errors/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-//.
-consteval int get_value(){
-    return 3;
-}
+
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value: " << value << std::endl;
+
+    //Compile Time Error - expected ';' 
+    // std::cout << "Hello World" << std::endl
+
+
+    std::cout << "Hello World!" << std::endl;
+    
+    //Runtime Error - gives us division by 0 warning
+    // int value = 7/0;
+    // std::cout << value << std::endl; 
+
     return 0;
 }
---
+++ Section_07/7.032_Errors/notes.md
@@ -0,0 +1,12 @@
+## Errors - C++
+
+
+### Three Types of Errors:
+
+- Compile Time Errors:
+    - Example: forgetting a semicolon, not defining the type of a data structure.
+- Runtime Errors:
+    - Compilation succeeds, but program does not perform as expected. Usually due to logic problem. Can lead to program crashes.
+- Warnings:
+    - Not a serious enough problem to stop compilation, but lets you know that some behavior is potentially problematic. 
+
