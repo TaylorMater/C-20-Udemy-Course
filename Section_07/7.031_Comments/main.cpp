d05e11a323dfe77cb09ae018ddb830c2403060a1
TaylorMater <rt.taylor.riley@gmail.com>
2024-06-27 07:34:08 -0500

    demonstrated comments for 7.031

--- Section_07/7.031_Comments/main.cpp
+++ Section_07/7.031_Comments/main.cpp
@@ -1,11 +1,20 @@
+
+//This brings in the iostream library
 #include <iostream>
-//.
-consteval int get_value(){
-    return 3;
-}
+
+/*
+
+    This is a block comment. You can highlight text and comment it out with CTRL + '/', but it will be individual line comments, to avoid nesting issues. You cannot nest block comments.
+
+*/
+
+// These are words that I am currently typing. 
+// A miracle, formed from millions and millions of years of a system gradually changing as energy is routinely thrust upon it and radiated back: basic life forms evolving into more complex specimens and gradually building into the ones of which I am a member. 
+// We possess, arguably most potently, the capacity for communication, understanding of self, and the capacity to retain information we learn from our environment. 
+// It is truly miraculous in this context, the whole of my own existence. 
 
 int main(){
-    constexpr int value = get_value();
-    std::cout << "value: " << value << std::endl;
+    //this is going to print "Hello World" to the console
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
